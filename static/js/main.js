ed0cf19179c494045df1e1503b80a91978c43886
Zaytsev_EV <Zaytsev_EV@directum.ru>
2018-07-25 20:06:35 +0400

    some front rework

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,9 @@ var page = 1;
 var persons = [];
 
 $(document).ready(function() {
+
+	new ClipboardJS('.btn_copy');
+
 	let $grid = $('.grid').masonry({
 		itemSelector: '.grid-item',
 		fitWidth: true,
@@ -77,14 +80,15 @@ function inputData(data) {
 
 	for (let i = 0; i < allPersons.length; i++) {
 		let person = allPersons[i].toString().split(',');
-		let htmlOutput = `<label><div class="person-block"><input type="checkbox" id="${person[0]}">${person[0]}<span>${person[1]}</span></div></label>`;
+        let htmlOutput = '<label><div class="person-block"><input type="checkbox" id="' + person[0] + '">' + person[0] + '<span>' + person[1] + '</span></div></label>';
 
 		jFacetBlock.append(htmlOutput);
 	}
 
-	var html = [];
+	let html = [];
 	for (let i = 0; i < photo.length; i++) {
-		html.push('<div class="grid-item"><img src="' + photo[i] + '"></div>');
+		let path = photo[i].replace('\\thumbnails', '');
+		html.push('<div class="grid-item"><img src="' + photo[i] + '"><a class="btn_copy" data-clipboard-text="' + path + '">Copy link</a></div>');
 	}
 
 	var $items = $(html.join('\n'));
@@ -96,10 +100,11 @@ function inputData(data) {
 	}, 500);
 
 	$('.person-block input').on('change', function() {
+		$('#photoBlock').animate({scrollTop:0}, 500, 'swing');
 		if ($(this).is(':checked')) {
 			persons.push($(this).attr('id'));
 		} else {
-			persons.splice($.inArray($(this).attr('id'), persons) ,1);
+			persons.splice( $.inArray($(this).attr('id'), persons) ,1 );
 		}
 		clear();
 		sendRequest(collectData());
